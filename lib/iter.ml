62d174ed7e0d70baafd65eae5e692f1c1da32728
NAlec <alexandre_gouraud@orange.fr>
2024-07-19 22:31:16 +0200

    Import files

---
+++ lib/iter.ml
@@ -0,0 +1,212 @@
+open Bigarray
+
+(**
+    Given an index {math i(i_0,i_1, ..., i_n-1)} of dimension {math d(d_0, d_1, ..., d_n-1)} such that 
+    {math \forall k \epsilon [0;n-1] 0 <= i_k < d_k}, the function increment the index [i] according to the following recursive algorithm: 
+
+    if {math i_0 < n_0 - 1} then {math i_0 = i_0 + 1} else {math i_0=0} and i_1 is incremented by one according the same alogrithm. 
+
+    @param i an array of size n corresponding to an index of an n dimensions Genarray
+    @param dims an array of size n giving all the dimensions of the index i : 
+    i.(0) goes from 0 to dims.(0)
+    i.(1) goes from 0 to dims.(1)
+    etc.
+    *)
+let incr i dims = 
+  let l = Array.length i in 
+  let rec incr_jth k dims j n = 
+    if (k.(j) + 1) < dims.(j) then 
+      begin
+        k.(j) <- k.(j) + 1; 
+        true
+      end
+    else
+      begin
+        k.(j) <- 0;
+        if (j+1) < n then 
+          incr_jth k dims (j+1) n
+        else 
+          false 
+      end 
+    in 
+  incr_jth i dims 0 l
+
+(**
+    Increment the index i by one. 
+    @param i an array of size n corresponding to an index of an n dimensions Genarray
+    @param dims an array of size n giving all the dimensions of the index i : 
+    i.(0) goes from 0 to dims.(0)
+    i.(1) goes from 0 to dims.(1)
+    etc...
+*)
+  let incr_last i dims = 
+    let l = Array.length i in 
+    let rec incr_jth i dims j = 
+      if (i.(j) + 1) < dims.(j) then 
+        begin
+          i.(j) <- i.(j) + 1; 
+          true
+        end
+      else
+        begin
+          i.(j) <- 0;
+          if j > 0 then 
+            incr_jth i dims (j-1) 
+          else 
+            false 
+        end 
+      in 
+    incr_jth i dims (l-1)
+
+(**
+      Same as [incr] but decrementing instead
+*)
+let decr i dims = 
+  let rec decr_jth k dims j = 
+    if k.(j) > 0 then 
+      begin
+        k.(j) <- k.(j) - 1; 
+        true
+      end
+    else
+      begin
+        k.(j) <- dims.(j) - 1;
+        if (j+1) < (Array.length i) then 
+          decr_jth k dims (j+1)
+        else 
+          false 
+      end 
+    in 
+  decr_jth i dims 0
+ 
+  let decr_last i dims = 
+    let l = Array.length i in 
+    let rec decr_jth k dims j = 
+      if k.(j) > 0 then 
+        begin
+          k.(j) <- k.(j) - 1; 
+          true
+        end
+      else
+        begin
+          k.(j) <- dims.(j) - 1;
+          if j > 0 then 
+            decr_jth k dims (j-1) 
+          else 
+            false 
+        end 
+      in 
+    decr_jth i dims (l-1)
+
+(**
+    `iter f a` applies the function f in turn to all elements of `a`. It is equivalent to : 
+    `f a.(0); f a.(1); ... ; f a.(length a - 1);()`.
+    @param 1 The function to apply to each of the elements of the array, in place. Its type is 'a -> unit 
+    @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
+    *)
+let iter f a = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.make n 0 in 
+  let rec iter_ith f a i =
+    begin 
+      f (Genarray.get a i);
+      if incr i dims then iter_ith f a i
+    end
+    in 
+  iter_ith f a index 
+
+
+(**
+    Same as `iter`, but the function is applied to the index of the elemnt as first argument, and the element itself as second argument.
+    @param 1 The function to apply to each of the elements of the array, in place. Its type is int Array.t -> 'a -> unit 
+    @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
+    *)
+let iteri f a = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.make n 0 in 
+  let rec iter_ith f a i =
+    begin 
+      f i (Genarray.get a i);
+      if incr i dims then iter_ith f a i
+    end
+    in 
+  iter_ith f a index 
+
+
+(**
+    `map f a` applies the function f in turn to all elements of `a` and builds an array with the results returned by f: 
+    `[|a.(0); f a.(1); ... ; f a.(length a - 1)|]`
+    @param 1 The function to apply to each of the elements of the array, in place. Its type is 'a -> 'b 
+    @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
+    @return The newly created array of type : ('b, 'c, 'd) Genarray.t
+    *)
+let map f new_kind a = 
+  let dims = Genarray.dims a in 
+  let map_a2b i = f (Genarray.get a i)  in 
+  Genarray.init new_kind (Genarray.layout a) dims map_a2b  
+
+
+(**
+    Applies the function f to all elements of the Genarray a
+    @param 1 The function to apply to each of the elements of the array, in place. Its type is 'a -> 'a 
+    @param 2 The Genarray to which the function f is to be applied : has type 'a Genarray.t 
+    *)
+let map_inplace f a = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.make n 0 in 
+  let rec iter_ith f a i =
+    begin 
+      Genarray.set a i (f (Genarray.get a i));
+      if incr i dims then iter_ith f a i
+    end
+    in 
+  iter_ith f a index 
+
+(**
+    Same as `map` but the function is applied to the index of the element as first argument, and the element itself as second argument.  
+    @param 1 The function to apply to each of the elements of the array, in place. Its type is: `int Array.t -> 'a -> 'b`
+    @param 2 The Genarray to which the function f is to be applied. Its type is :  `('a, 'c, 'd) Genarray.t`
+    @return The newly created array of type : `('b, 'c, 'd) Genarray.t`
+    *)
+let mapi f new_kind a = 
+  let dims = Genarray.dims a in 
+  let map_a2b i = f i (Genarray.get a i)  in 
+  Genarray.init new_kind (Genarray.layout a) dims map_a2b  
+
+
+let mapi_inplace f a = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.make n 0 in 
+  let rec iter_ith f a i = 
+    Genarray.set a i (f i (Genarray.get a i));
+    if incr i dims then iter_ith f a i in 
+  iter_ith f a index 
+
+
+let fold_left f init a = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.make n 0 in 
+  let rec accumulate f init a =
+    let acc =  f init (Genarray.get a index) in 
+    match incr index dims with 
+        true -> accumulate f acc a 
+      | false -> acc 
+    in 
+  accumulate f init a 
+
+let fold_right f a init = 
+  let n = Genarray.num_dims a in 
+  let dims = Genarray.dims a in 
+  let index = Array.init n (fun i -> dims.(i)-1) in 
+  let rec accumulate f a init =
+    let acc =  f (Genarray.get a index) init in 
+    match decr index dims with 
+        true -> accumulate f a acc
+      | false -> acc 
+    in 
+  accumulate f a init 
---
+++ test/testiter.ml
@@ -0,0 +1,92 @@
+open Bigarray
+open OUnit2
+open Stdlib
+
+let array_to_string a = 
+  let f acc a = 
+    Printf.sprintf "%s%d " acc a in 
+  Array.fold_left f "" a 
+
+let test_incr input output dim not_last = 
+  let incr_ok = GenArrayIter.Iter.incr input dim in 
+  assert_equal not_last incr_ok ~printer:(fun x -> if x then "true" else "false");
+  assert_equal input output ~printer:array_to_string 
+
+let test_decr index expected_output dim not_last = 
+  let decr_ok = GenArrayIter.Iter.decr index dim in 
+  assert_equal not_last decr_ok ~printer:(fun x -> if x then "true" else "false");
+  assert_equal expected_output index ~printer:array_to_string   
+
+let test_incr_last index expected_output dim not_last = 
+  let incr_ok = GenArrayIter.Iter.incr_last index dim in 
+  assert_equal not_last incr_ok ~printer:(fun x -> if x then "true" else "false");
+  assert_equal expected_output index ~printer:array_to_string 
+
+let test_decr_last index expected_output dim not_last = 
+  let decr_ok = GenArrayIter.Iter.decr_last index dim in
+  assert_equal not_last decr_ok ~printer:(fun x -> if x then "true" else "false");
+  assert_equal expected_output index ~printer:array_to_string
+
+let test_mapi_inplace = 
+  let tab = Genarray.init int32 c_layout [|4;2;3|] (fun _ -> 0l) in 
+    let sum_index i _ = Int32.of_int (Array.fold_left (fun acc x -> (acc + x) ) 0 i) in 
+    GenArrayIter.Iter.mapi_inplace sum_index tab;
+    assert_equal 6l (Genarray.get tab [|3;1;2|]) ~printer:(Printf.sprintf "%ld")
+
+let test_map_inplace = 
+  let tab = Genarray.init int32 c_layout [|4;2;3|] (fun _ -> 0l) in 
+  GenArrayIter.Iter.map_inplace (fun x -> Int32.add x 2l) tab;
+  assert_equal 2l (Genarray.get tab [|1;1;1|]) ~printer:(Printf.sprintf "%ld")
+
+
+let test_map = 
+  let tab = Genarray.init int32 c_layout [|4;2;3|] (fun _ -> 0l) in 
+  let mapped_tab = GenArrayIter.Iter.map (fun x -> Int32.add x 2l) int32 tab in 
+  assert_equal 2l (Genarray.get mapped_tab [|1;1;1|]) ~printer:(Printf.sprintf "%ld")
+
+let test_mapi = 
+  let tab = Genarray.init int c_layout [|4;2;3|] (fun _ -> 7) in 
+  let sum_index i j = Array.fold_left (fun x acc -> (x + acc)) j i in 
+  let mapped_tab = GenArrayIter.Iter.mapi sum_index int tab in 
+  assert_equal 10 (Genarray.get mapped_tab [|1;1;1|]) ~printer:(Printf.sprintf "%d");
+  assert_equal 7 (Genarray.get mapped_tab [|0;0;0|]) ~printer:(Printf.sprintf "%d") 
+
+let test_fold_left = 
+  let tab = Genarray.init int c_layout [|2;2;2|] (fun _ -> 0) in 
+  let f i j = i.(0) + 2 * i.(1) + 4 * i.(2) in 
+  let mapped_tab = GenArrayIter.Iter.mapi f int tab in (* fills in the map with integers from 0 to 2^3-1*) 
+  let sum = GenArrayIter.Iter.fold_left (fun x acc -> (x + acc)) 0 mapped_tab in  (* sum all intergers from 0 to 7 : 7*8/2 = 28 *)
+  assert_equal 28 sum ~printer:(Printf.sprintf "%d") 
+
+let test_fold_right = 
+  let tab = Genarray.init int c_layout [|2;2;2|] (fun _ -> 0) in 
+  let f i j = i.(0) + 2 * i.(1) + 4 * i.(2) in 
+  let mapped_tab = GenArrayIter.Iter.mapi f int tab in (* fills in the map with integers from 0 to 2^3-1*) 
+  let sum = GenArrayIter.Iter.fold_right (fun x acc -> (x + acc)) mapped_tab 0 in  (* sum all intergers from 0 to 7 : 7*8/2 = 28 *)
+  assert_equal 28 sum ~printer:(Printf.sprintf "%d") 
+
+let iter_test_suite = 
+  let dim = [|4;2;3|] in  
+  "test suite for iter"             >::: [
+  "01 test incr first value"           >:: (fun _ -> test_incr      [|0;0;0|] [|1;0;0|] dim true);
+  "02 test incr end of first dim"      >:: (fun _ -> test_incr      [|3;0;0|] [|0;1;0|] dim true);
+  "03 test incr last dim"              >:: (fun _ -> test_incr      [|3;1;2|] [|0;0;0|] dim false);
+  "04 test incr_last first value"      >:: (fun _ -> test_incr_last [|0;0;0|] [|0;0;1|] dim true);
+  "05 test incr_last end of first dim" >:: (fun _ -> test_incr_last [|0;0;2|] [|0;1;0|] dim true);
+  "06 test incr_last last dim"         >:: (fun _ -> test_incr_last [|3;1;2|] [|0;0;0|] dim false);
+  "07 test decr first value"           >:: (fun _ -> test_decr      [|3;1;2|] [|2;1;2|] dim true);
+  "08 test decr end of first dim"      >:: (fun _ -> test_decr      [|0;1;2|] [|3;0;2|] dim true);
+  "09 test decr last dim"              >:: (fun _ -> test_decr      [|0;0;0|] [|3;1;2|] dim false);
+  "10 test decr_last first value"      >:: (fun _ -> test_decr_last [|3;1;2|] [|3;1;1|] dim true);
+  "11 test decr_last end of first dim" >:: (fun _ -> test_decr_last [|3;1;0|] [|3;0;2|] dim true);
+  "12 test decr_last last dim"         >:: (fun _ -> test_decr_last [|0;0;0|] [|3;1;2|] dim false);
+  "13 test mapi_inplace"               >:: (fun _ -> test_mapi_inplace);
+  "14 test map_inplace"                >:: (fun _ -> test_map_inplace);
+  "15 test map"                        >:: (fun _ -> test_map);
+  "16 test mapi"                       >:: (fun _ -> test_mapi);
+  "17 test fold_left"                  >:: (fun _ -> test_fold_left);
+  "18 test fold_right"                 >:: (fun _ -> test_fold_right)
+
+]
+
+let _ = run_test_tt_main iter_test_suite

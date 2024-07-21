fad5c30fd0149bc9457edb5033af6d09c706d614
NAlec <alexandre_gouraud@orange.fr>
2024-07-21 13:26:25 +0200

    .gitignore and .ocamlformat added, as well a formatting all files along .ocamlformat template

--- lib/iter.ml
+++ lib/iter.ml
@@ -12,23 +12,16 @@ open Bigarray
     i.(1) goes from 0 to dims.(1)
     etc.
     *)
-let incr i dims = 
-  let l = Array.length i in 
-  let rec incr_jth k dims j n = 
-    if (k.(j) + 1) < dims.(j) then 
-      begin
-        k.(j) <- k.(j) + 1; 
-        true
-      end
-    else
-      begin
-        k.(j) <- 0;
-        if (j+1) < n then 
-          incr_jth k dims (j+1) n
-        else 
-          false 
-      end 
-    in 
+let incr i dims =
+  let l = Array.length i in
+  let rec incr_jth k dims j n =
+    if k.(j) + 1 < dims.(j) then (
+      k.(j) <- k.(j) + 1;
+      true)
+    else (
+      k.(j) <- 0;
+      if j + 1 < n then incr_jth k dims (j + 1) n else false)
+  in
   incr_jth i dims 0 l
 
 (**
@@ -39,64 +32,43 @@ let incr i dims =
     i.(1) goes from 0 to dims.(1)
     etc...
 *)
-  let incr_last i dims = 
-    let l = Array.length i in 
-    let rec incr_jth i dims j = 
-      if (i.(j) + 1) < dims.(j) then 
-        begin
-          i.(j) <- i.(j) + 1; 
-          true
-        end
-      else
-        begin
-          i.(j) <- 0;
-          if j > 0 then 
-            incr_jth i dims (j-1) 
-          else 
-            false 
-        end 
-      in 
-    incr_jth i dims (l-1)
+let incr_last i dims =
+  let l = Array.length i in
+  let rec incr_jth i dims j =
+    if i.(j) + 1 < dims.(j) then (
+      i.(j) <- i.(j) + 1;
+      true)
+    else (
+      i.(j) <- 0;
+      if j > 0 then incr_jth i dims (j - 1) else false)
+  in
+  incr_jth i dims (l - 1)
 
 (**
       Same as [incr] but decrementing instead
 *)
-let decr i dims = 
-  let rec decr_jth k dims j = 
-    if k.(j) > 0 then 
-      begin
-        k.(j) <- k.(j) - 1; 
-        true
-      end
-    else
-      begin
-        k.(j) <- dims.(j) - 1;
-        if (j+1) < (Array.length i) then 
-          decr_jth k dims (j+1)
-        else 
-          false 
-      end 
-    in 
+let decr i dims =
+  let rec decr_jth k dims j =
+    if k.(j) > 0 then (
+      k.(j) <- k.(j) - 1;
+      true)
+    else (
+      k.(j) <- dims.(j) - 1;
+      if j + 1 < Array.length i then decr_jth k dims (j + 1) else false)
+  in
   decr_jth i dims 0
- 
-  let decr_last i dims = 
-    let l = Array.length i in 
-    let rec decr_jth k dims j = 
-      if k.(j) > 0 then 
-        begin
-          k.(j) <- k.(j) - 1; 
-          true
-        end
-      else
-        begin
-          k.(j) <- dims.(j) - 1;
-          if j > 0 then 
-            decr_jth k dims (j-1) 
-          else 
-            false 
-        end 
-      in 
-    decr_jth i dims (l-1)
+
+let decr_last i dims =
+  let l = Array.length i in
+  let rec decr_jth k dims j =
+    if k.(j) > 0 then (
+      k.(j) <- k.(j) - 1;
+      true)
+    else (
+      k.(j) <- dims.(j) - 1;
+      if j > 0 then decr_jth k dims (j - 1) else false)
+  in
+  decr_jth i dims (l - 1)
 
 (**
     `iter f a` applies the function f in turn to all elements of `a`. It is equivalent to : 
@@ -104,36 +76,30 @@ let decr i dims =
     @param 1 The function to apply to each of the elements of the array, in place. Its type is 'a -> unit 
     @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
     *)
-let iter f a = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.make n 0 in 
+let iter f a =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.make n 0 in
   let rec iter_ith f a i =
-    begin 
-      f (Genarray.get a i);
-      if incr i dims then iter_ith f a i
-    end
-    in 
-  iter_ith f a index 
-
+    f (Genarray.get a i);
+    if incr i dims then iter_ith f a i
+  in
+  iter_ith f a index
 
 (**
     Same as `iter`, but the function is applied to the index of the elemnt as first argument, and the element itself as second argument.
     @param 1 The function to apply to each of the elements of the array, in place. Its type is int Array.t -> 'a -> unit 
     @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
     *)
-let iteri f a = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.make n 0 in 
+let iteri f a =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.make n 0 in
   let rec iter_ith f a i =
-    begin 
-      f i (Genarray.get a i);
-      if incr i dims then iter_ith f a i
-    end
-    in 
-  iter_ith f a index 
-
+    f i (Genarray.get a i);
+    if incr i dims then iter_ith f a i
+  in
+  iter_ith f a index
 
 (**
     `map f a` applies the function f in turn to all elements of `a` and builds an array with the results returned by f: 
@@ -142,28 +108,25 @@ let iteri f a =
     @param 2 The Genarray to which the function f is to be applied : has type ('a, 'c, 'd) Genarray.t 
     @return The newly created array of type : ('b, 'c, 'd) Genarray.t
     *)
-let map f new_kind a = 
-  let dims = Genarray.dims a in 
-  let map_a2b i = f (Genarray.get a i)  in 
-  Genarray.init new_kind (Genarray.layout a) dims map_a2b  
-
+let map f new_kind a =
+  let dims = Genarray.dims a in
+  let map_a2b i = f (Genarray.get a i) in
+  Genarray.init new_kind (Genarray.layout a) dims map_a2b
 
 (**
     Applies the function f to all elements of the Genarray a
     @param 1 The function to apply to each of the elements of the array, in place. Its type is 'a -> 'a 
     @param 2 The Genarray to which the function f is to be applied : has type 'a Genarray.t 
     *)
-let map_inplace f a = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.make n 0 in 
+let map_inplace f a =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.make n 0 in
   let rec iter_ith f a i =
-    begin 
-      Genarray.set a i (f (Genarray.get a i));
-      if incr i dims then iter_ith f a i
-    end
-    in 
-  iter_ith f a index 
+    Genarray.set a i (f (Genarray.get a i));
+    if incr i dims then iter_ith f a i
+  in
+  iter_ith f a index
 
 (**
     Same as `map` but the function is applied to the index of the element as first argument, and the element itself as second argument.  
@@ -171,42 +134,37 @@ let map_inplace f a =
     @param 2 The Genarray to which the function f is to be applied. Its type is :  `('a, 'c, 'd) Genarray.t`
     @return The newly created array of type : `('b, 'c, 'd) Genarray.t`
     *)
-let mapi f new_kind a = 
-  let dims = Genarray.dims a in 
-  let map_a2b i = f i (Genarray.get a i)  in 
-  Genarray.init new_kind (Genarray.layout a) dims map_a2b  
+let mapi f new_kind a =
+  let dims = Genarray.dims a in
+  let map_a2b i = f i (Genarray.get a i) in
+  Genarray.init new_kind (Genarray.layout a) dims map_a2b
 
-
-let mapi_inplace f a = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.make n 0 in 
-  let rec iter_ith f a i = 
+let mapi_inplace f a =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.make n 0 in
+  let rec iter_ith f a i =
     Genarray.set a i (f i (Genarray.get a i));
-    if incr i dims then iter_ith f a i in 
-  iter_ith f a index 
+    if incr i dims then iter_ith f a i
+  in
+  iter_ith f a index
 
-
-let fold_left f init a = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.make n 0 in 
+let fold_left f init a =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.make n 0 in
   let rec accumulate f init a =
-    let acc =  f init (Genarray.get a index) in 
-    match incr index dims with 
-        true -> accumulate f acc a 
-      | false -> acc 
-    in 
-  accumulate f init a 
+    let acc = f init (Genarray.get a index) in
+    match incr index dims with true -> accumulate f acc a | false -> acc
+  in
+  accumulate f init a
 
-let fold_right f a init = 
-  let n = Genarray.num_dims a in 
-  let dims = Genarray.dims a in 
-  let index = Array.init n (fun i -> dims.(i)-1) in 
+let fold_right f a init =
+  let n = Genarray.num_dims a in
+  let dims = Genarray.dims a in
+  let index = Array.init n (fun i -> dims.(i) - 1) in
   let rec accumulate f a init =
-    let acc =  f (Genarray.get a index) init in 
-    match decr index dims with 
-        true -> accumulate f a acc
-      | false -> acc 
-    in 
-  accumulate f a init 
+    let acc = f (Genarray.get a index) init in
+    match decr index dims with true -> accumulate f a acc | false -> acc
+  in
+  accumulate f a init
--- lib/iter.mli
+++ lib/iter.mli
@@ -5,7 +5,7 @@ open Bigarray
     To make these operations possible, an increment/decrement function is needed. There are many ways to iterate over multi-dimension spaces, but here two versions are provided : [incr] and [incr_last] (with their corresponding [decr] and [decr_last] versions).  
  *)
 
-
+val incr : int array -> int array -> bool
 (**
     Given an index {m i(i_0,i_1, ..., i_{n-1})} of dimension {m d(d_0, d_1, ..., d_{n-1})} such that 
     {m \forall k \in [0;n-1]} with {m 0 \leqslant i_k < d_k}, the function increments the index {m i} according to the following recursive algorithm: 
@@ -33,57 +33,57 @@ open Bigarray
     - [index.(1)] goes from [0] to [dims.(1)]
     @return True if the increment succeded. False otherwise, meaning the index has reached the end of the array.
 *)
-val incr : int array -> int array -> bool
 
-
-
+val incr_last : int array -> int array -> bool
 (**
     Same as [incr] but instead of incrementing the first dimension ({m i_0}) of the index {m i(i_0, ..., i_{n-1})}, increments the last dimension first ({m i_{n-1}})
 *)
-val incr_last : int array -> int array -> bool
 
+val decr : int array -> int array -> bool
 (**
     Same as [incr] but in decrementing instead of incrementing.
 *)
-val decr : int array -> int array -> bool
 
+val decr_last : int array -> int array -> bool
 (**
     Same as [decr] but instead of decrementing the first dimention ({m i_0}) of the index {m i(i_0, ..., i_{n-1})}, decrements the last dimension first ({m i_{n-1}})
 *)
-val decr_last : int array -> int array -> bool
 
+val iter : ('a -> unit) -> ('a, 'b, 'c) Genarray.t -> unit
 (**
     [iter f a] applies the function [f] in turn to each element of the array [a]. 
     It is equivalent to [f a.(0); f a.(1); ... ; f a.(length a - 1); ()]
     @param 1 [f] : a function that takes element of the array (second argument) in inputs. 
     @param 2 [a] : The array on the elements of which the function [f] is applied. 
 *)
-val iter : ('a -> unit) -> ('a, 'b, 'c) Genarray.t -> unit
 
-
-
+val iteri : (int Array.t -> 'a -> unit) -> ('a, 'b, 'c) Genarray.t -> unit
 (**
     Same as [iter] except that the function [f] is applied to the index of the array as first argument, and to the element itself as second argument. 
     [iteri f a] applies the function [f] in turn to each element of the array [a] and the index [i] : [f i a.(i)] for all possibles values of the index i.  
 
     {b NB}: the index is incremented using the function [Iter.incr]
 *)
-val iteri : (int Array.t -> 'a -> unit) -> ('a, 'b, 'c) Genarray.t -> unit
 
-
-
+val map :
+  ('a -> 'b) ->
+  ('b, 'k2) kind ->
+  ('a, 'k1, 'layout) Genarray.t ->
+  ('b, 'k2, 'layout) Genarray.t
 (**
    [map f k a] applies the map function [f] in turn to each element of the genarray [a] of type ['a] and outputs another genarray [b], elements of which have type ['b].
 *)
-val map : ('a -> 'b) -> ('b, 'k2) kind -> ('a,'k1,'layout) Genarray.t -> ('b, 'k2, 'layout) Genarray.t  
 
-
+val map_inplace : ('a -> 'a) -> ('a, 'b, 'c) Genarray.t -> unit
 (**
     Same as [map] but modifies the genarray {i inplace} instead of creating a new one.
 *)
-val map_inplace : ('a -> 'a) -> ('a,'b,'c) Genarray.t -> unit 
 
-
+val mapi :
+  (int Array.t -> 'a -> 'b) ->
+  ('b, 'k2) kind ->
+  ('a, 'k1, 'layout) Genarray.t ->
+  ('b, 'k2, 'layout) Genarray.t
 (**
     Same as [map] except that the function [f] is applied to the index of the array as the first argument, and to the element itself as second argument.
     [mapi f k a] applies the function [f] in turn to each element of the array [a] and the index [i] : [f i a.(i)] for all possibles values of the index [i]. 
@@ -91,29 +91,29 @@ val map_inplace : ('a -> 'a) -> ('a,'b,'c) Genarray.t -> unit
 
     {b NB} : the function [incr] is used to iterate over all elements of the array [a]
 *)
-val mapi : (int Array.t -> 'a -> 'b) -> ('b, 'k2) kind -> ('a,'k1,'layout) Genarray.t -> ('b, 'k2, 'layout) Genarray.t 
 
+val mapi_inplace : (int Array.t -> 'a -> 'a) -> ('a, 'b, 'c) Genarray.t -> unit
 (**
     A combination of [mapi] and [map_inplace] : [mapi_inplace f a] applies [f] in turn to each index [i] (as first argument) and element of the array [a] (as second argument).
 
     {b NB}: the function [incr] is used to iterate over all elements of the array [a]
 *)
-val mapi_inplace : (int Array.t -> 'a -> 'a) -> ('a,'b,'c) Genarray.t -> unit 
 
 (*
 val fold_left : ('acc -> 'a -> 'acc) -> 'acc -> ('a, 'b, 'layout) Genarray.t -> 'acc
 val fold_right: ('a -> 'acc -> 'acc) -> ('a, 'b, 'layout) Genarray.t -> 'acc -> 'acc
 *)
 
-
+val fold_left :
+  ('acc -> 'a -> 'acc) -> 'acc -> ('a, 'b, 'layout) Genarray.t -> 'acc
 (**
     [fold_left f init a] computes [f (f (... (f init a.(0)) ... ) a.(n-1))] where [n] is the length of the genarray [a].
     {b NB} : the function [incr] is used to iterate over all elements of the array [a]
 *)
-val fold_left : ('acc -> 'a -> 'acc) -> 'acc -> ('a, 'b, 'layout) Genarray.t -> 'acc
 
+val fold_right :
+  ('a -> 'acc -> 'acc) -> ('a, 'b, 'layout) Genarray.t -> 'acc -> 'acc
 (**
     [fold_right f a init] computes [f a.(0) (f a.(1) (... (f a.(n-1) init)))] where [n] is the length of the genarray [a].
     {b NB} : the function [decr] is used to iterate over all elements of the array [a]
 *)
-val fold_right : ('a -> 'acc -> 'acc) -> ('a, 'b, 'layout) Genarray.t -> 'acc -> 'acc
